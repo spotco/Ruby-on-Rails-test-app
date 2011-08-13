e49946aafb16b2939f7d69c65de91c3a48b6f424
spotco <mootothemax@gmail.com>
2011-08-13 14:16:22 -0700

    encryptpass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,9 @@ class UsersController < ApplicationController
   def new
 	@title = "Enregister"
   end
+  
+  def show
+	@user = User.find(params[:id])
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,34 @@
 class User < ActiveRecord::Base
+	attr_accessor :password
 	attr_accessible :name, :email
 	
 	email_regex = /^[\w]+[@]{1}[\w]+[.]{1}[\w]+$/
 	
-	validates( :name,  :presence => true,
-					   :length => { :maximum => 4 } )
-					  
-	validates( :email, :presence => true,
+	validates  :name,  :presence => true,
+					   :length => { :maximum => 20 } 
+					   
+	validates  :email, :presence => true,
 					   :format => {:with => email_regex },
-					   :uniqueness => { :case_sensitive => false})
+					   :uniqueness => { :case_sensitive => false}
+					   
+	validates  :password, :presence => true,
+	                      :confirmation => true,
+	                      :length => { :within => 6..40 }
+	                     
 end
 
 #user.errors.full_messages
+
 
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
