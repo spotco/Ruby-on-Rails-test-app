caa2b6e43a7dbc754e8ed8ae3b45100fbda03081
spotco <mootothemax@gmail.com>
2011-08-13 15:23:11 -0700

    password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,54 @@
 class User < ActiveRecord::Base
 	attr_accessor :password
-	attr_accessible :name, :email
+	attr_accessible :name, :email, :password, :password_confirmation, :salt
 	
 	email_regex = /^[\w]+[@]{1}[\w]+[.]{1}[\w]+$/
 	
 	validates  :name,  :presence => true,
-					   :length => { :maximum => 20 } 
+					           :length => { :maximum => 20 } 
 					   
 	validates  :email, :presence => true,
-					   :format => {:with => email_regex },
-					   :uniqueness => { :case_sensitive => false}
+					           :format => {:with => email_regex },
+					           :uniqueness => { :case_sensitive => false}
 					   
 	validates  :password, :presence => true,
 	                      :confirmation => true,
 	                      :length => { :within => 6..40 }
+	                      
+	validates :password_confirmation, :presence => true
+	                      
+	                      
+	                      
+  before_save :encrypt_password
+  
+  def self.authenticate?(email,pass)
+    user = User.find_by_email(email)
+    
+    return nil if user.nil?
+    return user if user.has_password?(pass)
+  end
+  
+  def has_password?(submitted_password)
+     self.encrypted_password == encrypt(submitted_password) 
+  end
+  
+  private
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(self.password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def secure_hash(str)
+      Digest::SHA2.hexdigest(str)
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
 	                     
 end
 
