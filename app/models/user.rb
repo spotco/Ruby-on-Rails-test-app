eb7c8036e5f54693c51b4a6a0c4e9ff6d63e1e6c
spotco <mootothemax@gmail.com>
2011-08-13 02:17:11 -0700

    User model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email
+	
+	email_regex = /^[\w]+[@]{1}[\w]+[.]{1}[\w]+$/
+	
+	validates( :name,  :presence => true,
+					   :length => { :maximum => 4 } )
+					  
+	validates( :email, :presence => true,
+					   :format => {:with => email_regex },
+					   :uniqueness => { :case_sensitive => false})
 end
+
+#user.errors.full_messages
 
 # == Schema Information
 #
