986d2ed118477db7ea4d2e540dfd25214d82d780
spotco <mootothemax@gmail.com>
2011-08-14 00:55:55 -0700

    user signup working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,16 @@ class UsersController < ApplicationController
 	 @user = User.find(params[:id])
 	 @title = @user.name
   end
+  
+  def create
+    #raise params[:user].inspect
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to user_path(@user), :flash => { :success => "Welcome to sample app"}
+    else
+      @title = "Signup error!"
+      render :new
+    end
+  end
 
 end
